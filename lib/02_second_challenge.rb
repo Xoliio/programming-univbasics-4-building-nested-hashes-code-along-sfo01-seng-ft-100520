728fd33f99857ecc401ffc7fa37333f112ef4287
Inee Ader <xoliio@gmail.com>
2020-09-02 20:05:55 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,18 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch 
+      :patriarch
       :matriarch
       :hero
       :hero_friends
-      }, 
+      },
     :capulet => {
-      :patriarch 
+      :patriarch
       :matriarch
       :heroine
       :heroine_friends
       }
     }
 
-  
+
 end
